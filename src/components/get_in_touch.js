3fc7a0d5d146d3f3c8542f74a241ec471b2e730e
Andrew <acdickin@gmail.com>
2017-06-09 15:29:45 -0700

    working google form added to the get in touch page

--- src/components/get_in_touch.js
+++ src/components/get_in_touch.js
@@ -4,9 +4,9 @@ export default class GetInTouch extends Component {
   render() {
     return (
       <div className="flex-container">
-      	<div className="title-blk blk">
-      		<h2 className="title" >Get In Touch Page</h2>
-      	</div>
+      	<div className="form">
+      			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf1ux9HRe4PmEf9vgBNeii6v__0sMyCBE5Jg-4h2XI0rn5hYQ/viewform?embedded=true" width="100%" height="800px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+     		</div>
       </div>
     );
   }
