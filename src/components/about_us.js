0bbd7ceac8f86964bcc4c2cd993509d948edb3f5
Andrew Dickinson <andrew.dickinson@t2.local>
2017-06-29 15:22:18 -0700

    styling

--- src/components/about_us.js
+++ src/components/about_us.js
@@ -10,10 +10,11 @@ export default class AboutUs extends Component {
           <h2 className="title" > About Us</h2>
       		<Carousel images={IMGS.about}/>
       	</div>
-        <div className="blk">
-          <span>We’re millenials, so, of course, we met on Tinder, and it didn’t take long for Krystaal to plan a date. Our first date included three of our favorite things: hiking, pizza, and lots of laughter. From the start, we got along so easily, and loudly, feeling nearly immediate comfort with one another. Something to do with Andrew’s kind heartedness and Krystaal’s bossily caring nature. We knew very early on that we would be together for a long time.</span>
+        <div className="blk about-text">
           <br/>
-          <span>These days, we happily spend time snuggling on the couch with our cat Patapon, going hiking and camping, and cooking gluten-free, vegetarian meals together. This Washingtonian and Michigander match is one of noisy, adventurous, understanding love.</span>
+          <p>     We’re millenials, so, of course, we met on Tinder, and it didn’t take long for Krystaal to plan a date. Our first date included three of our favorite things: hiking, pizza, and lots of laughter. From the start, we got along so easily, and loudly, feeling nearly immediate comfort with one another. Something to do with Andrew’s kind heartedness and Krystaal’s bossily caring nature. We knew very early on that we would be together for a long time.</p>
+
+          <p>     These days, we happily spend time snuggling on the couch with our cat Patapon, going hiking and camping, and cooking gluten-free, vegetarian meals together. This Washingtonian and Michigander match is one of noisy, adventurous, understanding love.</p>
         </div>
 
       </div>
