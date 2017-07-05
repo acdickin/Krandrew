cc1dda24ec86d518c35bd7c56a7569c9755dc45d
Andrew Dickinson <andrew.dickinson@t2.local>
2017-07-05 09:16:31 -0700

    styling

--- src/components/blog.js
+++ src/components/blog.js
@@ -6,7 +6,26 @@ export default class Blog extends Component {
     	<div className="flex-container">
       	<div className="title-blk ">
       		<h2 className="title" >Blog Page</h2>
+          <div className="main about-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend bibendum magna, a volutpat ipsum consequat sit amet. Vestibulum tincidunt metus at neque sodales, at maximus lectus rhoncus. Nullam sit amet tincidunt nisi. Praesent aliquet sagittis aliquet. Donec vel lacus eget lacus interdum blandit. Maecenas non ullamcorper est, id gravida risus. Pellentesque ut enim in metus pellentesque interdum. Nunc in nunc ac dolor laoreet cursus in et est.
+            </p>
+            <p>
+            Praesent ex dui, placerat vel luctus sit amet, egestas nec felis. Aliquam aliquam elit eget felis sodales, eget varius velit consectetur. Maecenas et nulla quis lacus venenatis laoreet. Vivamus gravida ligula ac leo sagittis luctus. Nunc accumsan finibus sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum vulputate mollis orci nec interdum. Pellentesque imperdiet, purus sed commodo iaculis, ex enim laoreet nisi, eget sagittis urna augue luctus eros. Nulla in tincidunt sem. Pellentesque eget sapien quis elit porttitor finibus. Proin sollicitudin, lectus sit amet venenatis facilisis, nunc est aliquet diam, rhoncus pharetra orci sem nec dui.
+            </p>
+            <p>
+            Integer accumsan ullamcorper massa at cursus. Pellentesque malesuada sem in lacus blandit, quis posuere risus laoreet. Fusce ultrices sapien diam, id mattis risus imperdiet nec. Suspendisse vitae nibh velit. Morbi bibendum est eget aliquam porta. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque euismod in nisi in rhoncus. Sed dictum nisi ut lorem interdum ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin mollis ultrices interdum. Praesent dapibus lorem id quam consequat, id cursus velit viverra. Maecenas tortor magna, tempor quis erat sed, vulputate rhoncus diam. Aenean ut neque risus. Nunc id sollicitudin dolor.
+            </p>
+            <p>
+            Nullam at dignissim odio. Integer gravida aliquam turpis, eget congue leo feugiat vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar risus in orci volutpat placerat quis quis dui. Aenean et elit sed mauris consequat suscipit. Mauris sit amet velit at neque suscipit pulvinar. Fusce dapibus risus sapien, sit amet gravida libero accumsan a. Suspendisse eget mattis magna. Pellentesque luctus finibus tristique. Donec efficitur luctus orci sed luctus. Nulla consequat sapien nibh, at accumsan nunc placerat eget. Mauris varius, mi id sollicitudin aliquet, sem massa viverra elit, a tristique neque metus sit amet nibh. Pellentesque pulvinar aliquam urna, tincidunt scelerisque tellus vulputate at. Sed aliquet varius enim vitae tincidunt. Morbi molestie faucibus enim, eu eleifend enim condimentum et. Nam consequat lobortis augue, ut commodo velit blandit non.
+            </p>
+            <p>
+            Donec mattis ex ex, et lacinia odio condimentum id. Nullam porta, dolor id luctus pulvinar, massa erat vulputate quam, ut elementum erat dui ut urna. In volutpat justo quis diam luctus accumsan. Aliquam aliquet euismod dolor at suscipit. Donec lorem nibh, maximus vel rhoncus quis, viverra nec urna. Proin vulputate scelerisque ante sed elementum. Phasellus maximus viverra purus nec placerat.
+            </p>
+          </div>
       	</div>
+
+
       </div>
     );
   }
